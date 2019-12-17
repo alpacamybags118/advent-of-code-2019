d3beddaecfef4b019618908c12bd7eec79fccf7d
Kyle Robertson <kylerobertson@quickenloans.com>
2019-12-17 04:58:30 -0500

    started adding new functionality

--- day5/compute.js
+++ day5/compute.js
@@ -5,63 +5,70 @@ function ReadInput(path) {
     return fs.readFileSync(path, {encoding: "utf-8"});
 }
 
-function AddFunction(array, pos) {
-    array[array[pos+3]] = array[array[pos + 1]] + array[array[pos + 2]]
+function Compute(array, verb, noun) {
+    array[1] = noun;
+    array[2] = verb;
+    let index = 0;
+    let cont = array[index] != 99;
+
+    while(cont) {
+        codes = array[index].split(Math.floor(array[index].length / 2));
+        switch(codes[1]) {
+            case 1: array = AddFunction(array, index, mode); break;
+            case 2: array = MultiplyFunction(array, index, mode); break;
+            case 3: array = InputFunction(array, index, mode); break;
+            case 4: OutputFunction(array, index); break;
+            default: cont=false;
+        }
+
+        index += array[codes[1].length + codes[2].length];
+        if(cont) {
+            cont = array[index] != 99;
+        }
+    }
+
+    return array[0];
+}
+
+function AddFunction(array, pos, mode) {
+    const num1 = mode[mode.length - 1] == 0 ? Number(array[array[pos + 1]]) : Number(array[pos+1])
+    const num2 = mode[mode.length - 2] == 0 ? Number(array[array[pos + 2]]) : Number(array[pos+2])
+    mode[mode.length - 3] == 0 ? array[array[pos+3]] = num1 + num2 : array[pos+3] = num1 + num2
 
     return array
 }
 
-function MultiplyFunction(array,pos) {
-    array[array[pos+3]] = array[array[pos + 1]] * array[array[pos + 2]]
+function MultiplyFunction(array, pos, mode) {
+    const num1 = mode[mode.length - 1] == 0 ? Number(array[array[pos + 1]]) : Number(array[pos+1])
+    const num2 = mode[mode.length - 2] == 0 ? Number(array[array[pos + 2]]) : Number(array[pos+2])
+    mode[mode.length - 3] == 0 ? array[array[pos+3]] = num1 * num2 : array[pos+3] = num1 * num2
 
     return array
 }
 
-//const input = ReadInput(inputPath);
+function InputFunction(array, pos, mode) {
+    //need to look ahead for input
+    input
+}
+
+function OutputFunction(array, pos) {return array[pos];}
+
 const input = ReadInput(inputPath).split(',').map(x => Number(x))
-let index = 0;
+let val = 0
 let noun = 12; //increases final by a significant amount
 let verb = 2; //increases final by 1
 let iterations = 0;
+const final = 19690720
 
-//do a while unwinding
-while(verb < 99) {
-    while(noun  < 99)  {
-        let array = input.slice();
-        array[1] = noun;
-        array[2] = verb;
-        index = 0;
-        let cont = array[index] != 99;
-
-        while(cont) {
-            switch(array[index]) {
-                case 1:
-                    array = AddFunction(array, index);
-                    break;
-                case 2:
-                    array = MultiplyFunction(array, index);
-                    break;
-                default:
-                    cont = false;
-                    
-            }
-            index += 4;
-
-            if(array[index] == 99) {
-                cont = false;
-            }
-        }
-        iterations++;
-        if(array[0] == 19690720)  {
-            console.log("iterations - " + iterations)
-            console.log("answer: " + (100 * noun + verb))
-        }
-        else if(array[0] > 19690720) {
-            break
-        }
-
-        noun++;
-    }
-    verb++;
-    noun = 12;
+while(final > val) {
+    val = Compute(input.slice(), verb, ++noun);
+    iterations+=1;
 }
+noun--;
+while(final != val) {
+    val =  Compute(input.slice(), ++verb, noun);
+    iterations+=1;
+}
+
+console.log("Total iterations - " + iterations)
+console.log("Final answer - " + (100 * noun + verb))
